29cd000987b19d1dd9245e204be40d58844d58d1
MILENA ALCANTE LISBOA ACIOLE <milena.alaciole@senacsp.edu.br>
2025-01-16 14:48:34 -0300

    first commit

---
+++ lista_01/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+Ex.01 Verificação de paridade
+
+Objetivo: Pratique o uso do operador % (módulo) e estruturas condicionais simples. 
+Descrição: Crie um programa que solicite que o usuário digite um número inteiro. O programa deve verificar se o número é par ou ímpar. Use a condição if para determinar o resultado e exibir uma mensagem apropriada. 
+Exemplo de saída: "O número é par." ou "O número é ímpar".
+  */
